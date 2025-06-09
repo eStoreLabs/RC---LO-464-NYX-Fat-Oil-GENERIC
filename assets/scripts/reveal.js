62ce45ad607fbc9ca4d1fada031b7d8a68d129d6
Roman Pavlenko <rpavlenko.mail@gmail.com>
2025-06-09 09:52:37 +0200

    fix: add selector by id to js

--- assets/scripts/reveal.js
+++ assets/scripts/reveal.js
@@ -1,6 +1,6 @@
 (() => {
   function reveal() {
-    var reveals = document.querySelectorAll('.es-reveal');
+    var reveals = document.querySelectorAll('#es-rc #es-rc-content .es-reveal');
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
